ff8144ad4d80fa33ecc7c8159ea7a78af6319dd4
Danng <archiemclellan.author@gmail.com>
2026-02-05 18:28:09 +0000

    Create seo.tsx

---
+++ components/blocks/seo.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+
+export type SEOProps = {
+  title: string;
+  description?: string;
+  image?: string;
+  url?: string;
+};
+
+export default function SEO({ title, description, image, url }: SEOProps) {
+  const metaTitle = title;
+  const metaDescription = description ?? "";
+
+  return (
+    <Head>
+      <title>{metaTitle}</title>
+      <meta name="description" content={metaDescription} />
+
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content={metaTitle} />
+      <meta property="og:description" content={metaDescription} />
+      {image && <meta property="og:image" content={image} />}
+      {url && <meta property="og:url" content={url} />}
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={metaTitle} />
+      <meta name="twitter:description" content={metaDescription} />
+      {image && <meta name="twitter:image" content={image} />}
+    </Head>
+  );
+}
